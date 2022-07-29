99fcc5bba8046d8c375bed461b9d287232882079
Suji Oh <suji-inkblot@Sujis-MacBook-Pro.local>
2022-07-29 18:54:37 -0400

    better to learn python

---
+++ Array/longestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+ var lengthOfLongestSubstring = function (str) {
+     const hash = {};
+     let start = 0;
+     let max = 0;
+
+     for (let i = 0; i < str.length; i++) {
+         let rightChar = str[i];
+
+         if (!(rightChar in hash)) hash[rightChar] = 0;
+         hash[rightChar] += 1;
+
+         while (hash[rightChar] > 1) {
+             let leftChar = str[start];
+             start += 1;
+
+             if (leftChar in hash) hash[leftChar] -= 1;
+             if (hash[leftChar] === 0) delete hash[leftChar];
+         }
+         max = Math.max(max, i - start + 1);
+     }
+     return max;
+ };
+
+// Input: s = "abcabcbb"
+// Output: 3
+
+const string = "kwwkew";
+console.log(lengthOfLongestSubstring(string));
