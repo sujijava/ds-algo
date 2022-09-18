dfb1e2875958790e195d8d5e02040e8b8be4151c
Suji <sujioh5@gmail.com>
2022-09-17 22:05:19 -0400

    house rubber 2

--- Dynamic Programming/houseRobberII.js
+++ Dynamic Programming/houseRobberII.js
@@ -2,6 +2,31 @@
  * @param {number[]} nums
  * @return {number}
  */
- var rob = function(nums) {
-    
+
+var rob = function (nums) {
+  const len = nums.length;
+  if (len < 2) {
+    return nums[0] ? nums[0] : 0;
+  }
+
+  const arrA = nums.slice(0, len - 1);
+  const arrB = nums.slice(1, len);
+
+  return Math.max(houseRobberOne(arrA), houseRobberOne(arrB));
 };
+
+const houseRobberOne = (nums) => {
+  let prevPrevSum = 0;
+  let prevSum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    let temp = Math.max(prevPrevSum + nums[i], prevSum);
+    prevPrevSum = prevSum;
+    prevSum = temp;
+  }
+
+  return prevSum;
+};
+
+const testArr = [1, 2, 3, 1];
+console.log(rob(testArr));
